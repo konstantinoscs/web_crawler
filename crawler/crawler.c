de2e241be34a56ec9aaf34cab5876dfbb9c44421
konstantinoscs <kons.ste@gmail.com>
2018-06-02 04:16:46 +0300

    sockets from client side

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,10 +1,16 @@
+#include <arpa/inet.h>
+#include <ctype.h>
 #include <pthread.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#include "crawler.h"
 
 int set_socket(int port, int *sock){
   //int sock;
@@ -25,8 +31,13 @@ int set_socket(int port, int *sock){
   return 1;
 }
 
-void *thread_crawl(){
+int is_ip(char *host_or_ip){
+  return isdigit(host_or_ip[strlen(host_or_ip)]);
+}
 
+void *thread_crawl(void *info){
+  ThreadInfo *t_info = (ThreadInfo *) info;
+  printf("sizeof(server): %d\n", t_info->s_size);
 }
 
 int crawler_operate(char *host, char *save_dir, char *start_url, int c_port,
@@ -34,17 +45,47 @@ int crawler_operate(char *host, char *save_dir, char *start_url, int c_port,
 
   int c_sock, s_sock, newsock;
   pthread_t *threads = NULL;
+  //client variables
+  struct sockaddr_in server;
+  struct sockaddr *serverptr = (struct sockaddr *)&server;
+  struct hostent *rem;
+  struct in_addr host_address;
+  ThreadInfo t_info;
+
+
+  //set client
+  if(is_ip(host)){
+    inet_aton(host, &host_address);
+    rem = gethostbyaddr((const char*) &host_address, sizeof(host_address), AF_INET);
+  }
+  else{
+    if((rem = gethostbyname(host)) == NULL){
+      perror("Host by name: "); exit(-1);}
+  }
+
+  server.sin_family = AF_INET;
+  memcpy(&server.sin_addr, rem->h_addr, rem->h_length);
+  server.sin_port = htons(s_port);
+  t_info.s_size = sizeof(server);
+  t_info.serverptr = serverptr;
 
   if((threads = malloc(no_threads*sizeof(pthread_t))) == NULL){
     perror("threads malloc:"); exit(-2); }
 
   //create threads
   for(int i=0; i<no_threads; i++){
-    if(pthread_create(threads+i, NULL, thread_crawl, (void *)&i)){
+    if(pthread_create(threads+i, NULL, thread_crawl, (void *)&t_info)){
       perror("thread create:"); exit(-3); }
   }
   //set the socket
   set_socket(c_port, &c_sock);
+  //set client part
+
 
   //start crawling
+
+  for(int i=0; i<no_threads; i++)
+    if(pthread_join(threads[i], NULL)){
+      perror("pthread_join"); exit(1);}
+  free(threads);
 }
--- crawler/crawler.h
+++ crawler/crawler.h
@@ -4,4 +4,9 @@
 int crawler_operate(char *host, char *save_dir, char *start_url, int c_port,
   int s_port, int no_threads);
 
+typedef struct ThreadInfo{
+  int s_size;
+  struct sockaddr *serverptr;
+}ThreadInfo;
+
 #endif
